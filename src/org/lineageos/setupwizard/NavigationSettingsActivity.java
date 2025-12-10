LineageOS/android_packages_apps_SetupWizard

Revert "Hide navigation hint checkbox when taskbar is enabled"
This reverts commit a5ba5dfaad61864bff19f431d6ff7084decc2f42.

Change-Id: I567fb5dfaa8d937892b9333dd518ce30a9a4949b

src/org/lineageos/setupwizard/NavigationSettingsActivity.java
@@ -34,8 +34,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
 
     private SetupWizardApp mSetupWizardApp;
 
-    private boolean mIsTaskbarEnabled;
-
     private String mSelection = NAV_BAR_MODE_GESTURAL_OVERLAY;
 
     private CheckBox mHideGesturalHint;
@@ -49,8 +47,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
         if (mSetupWizardApp.getSettingsBundle().containsKey(DISABLE_NAV_KEYS)) {
             navBarEnabled = mSetupWizardApp.getSettingsBundle().getBoolean(DISABLE_NAV_KEYS);
         }
-        mIsTaskbarEnabled = LineageSettings.System.getInt(getContentResolver(),
-                LineageSettings.System.ENABLE_TASKBAR, isLargeScreen(this) ? 1 : 0) == 1;
 
         int deviceKeys = getResources().getInteger(
                 org.lineageos.platform.internal.R.integer.config_deviceHardwareKeys);
@@ -90,11 +86,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
         final RadioGroup radioGroup = findViewById(R.id.navigation_radio_group);
         mHideGesturalHint = findViewById(R.id.hide_navigation_hint);
 
-        // Hide navigation hint checkbox when taskbar is enabled
-        if (mIsTaskbarEnabled) {
-            mHideGesturalHint.setVisibility(View.GONE);
-        }
-
         radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
             switch (checkedId) {
                 case R.id.radio_gesture:
@@ -120,10 +111,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
     }
 
     private void revealHintCheckbox() {
-        if (mIsTaskbarEnabled) {
-            return;
-        }
-
         mHideGesturalHint.animate().cancel();
 
         if (mHideGesturalHint.getVisibility() == View.VISIBLE) {
@@ -139,10 +126,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
     }
 
     private void hideHintCheckBox() {
-        if (mIsTaskbarEnabled) {
-            return;
-        }
-
         if (mHideGesturalHint.getVisibility() == View.INVISIBLE) {
             return;
         }
@@ -162,12 +145,10 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
     @Override
     protected void onNextPressed() {
         mSetupWizardApp.getSettingsBundle().putString(NAVIGATION_OPTION_KEY, mSelection);
-        if (!mIsTaskbarEnabled) {
-            boolean hideHint = mHideGesturalHint.isChecked();
-            LineageSettings.System.putIntForUser(getContentResolver(),
-                    LineageSettings.System.NAVIGATION_BAR_HINT, hideHint ? 0 : 1,
-                    UserHandle.USER_CURRENT);
-        }
+        boolean hideHint = mHideGesturalHint.isChecked();
+        LineageSettings.System.putIntForUser(getContentResolver(),
+                LineageSettings.System.NAVIGATION_BAR_HINT, hideHint ? 0 : 1,
+                UserHandle.USER_CURRENT);
         super.onNextPressed();
     }
 

Mohithash/packages_apps_SetupWizard

Signed-off-by: Frost <frost.github@proton.me>

src/org/lineageos/setupwizard/NavigationSettingsActivity.java
@@ -35,8 +35,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
 
     private SetupWizardApp mSetupWizardApp;
 
-    private boolean mIsTaskbarEnabled;
-
     private String mSelection = NAV_BAR_MODE_GESTURAL_OVERLAY;
 
     private CheckBox mHideGesturalHint;
@@ -58,8 +56,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
 
         mSetupWizardApp = (SetupWizardApp) getApplication();
         boolean navBarEnabled = hasNavigationBar(this);
-        mIsTaskbarEnabled = Settings.System.getInt(getContentResolver(),
-                Settings.System.ENABLE_TASKBAR, isLargeScreen(this) ? 1 : 0) == 1;
 
         getGlifLayout().setDescriptionText(getString(R.string.navigation_summary));
         setNextText(R.string.next);
@@ -95,11 +91,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
         final RadioGroup radioGroup = findViewById(R.id.navigation_radio_group);
         mHideGesturalHint = findViewById(R.id.hide_navigation_hint);
 
-        // Hide navigation hint checkbox when taskbar is enabled
-        if (mIsTaskbarEnabled) {
-            mHideGesturalHint.setVisibility(View.GONE);
-        }
-
         radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
             switch (checkedId) {
                 case R.id.radio_gesture:
@@ -125,10 +116,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
     }
 
     private void revealHintCheckbox() {
-        if (mIsTaskbarEnabled) {
-            return;
-        }
-
         mHideGesturalHint.animate().cancel();
 
         if (mHideGesturalHint.getVisibility() == View.VISIBLE) {
@@ -144,10 +131,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
     }
 
     private void hideHintCheckBox() {
-        if (mIsTaskbarEnabled) {
-            return;
-        }
-
         if (mHideGesturalHint.getVisibility() == View.INVISIBLE) {
             return;
         }
@@ -167,12 +150,10 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
     @Override
     protected void onNextPressed() {
         mSetupWizardApp.getSettingsBundle().putString(NAVIGATION_OPTION_KEY, mSelection);
-        if (!mIsTaskbarEnabled) {
-            boolean hideHint = mHideGesturalHint.isChecked();
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NAVIGATION_BAR_HINT, hideHint ? 0 : 1,
-                    UserHandle.USER_CURRENT);
-        }
+        boolean hideHint = mHideGesturalHint.isChecked();
+        Settings.System.putIntForUser(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HINT, hideHint ? 0 : 1,
+                UserHandle.USER_CURRENT);
         super.onNextPressed();
     }
 